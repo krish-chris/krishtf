aa7148028481b6dfc51350b29b20d484d464c2d3
admin <kailashshrestha2018@gmail.com>
2025-05-31 18:21:16 +0545

    updated and working file

--- day 2/main.tf
+++ day 2/main.tf
@@ -8,18 +8,18 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-resource "aws_instance" "terra_instance" {
-    ami = ami-084568db4383264d4
-    instance_type = t2.micro
+resource "aws_instance" "terra_instance1" {
+    ami = "ami-084568db4383264d4"
+    instance_type = "t2.micro"
     provider = aws.region1
     tags = {
         Name : "tfinstance1"
     }
 }
 
-resource "aws_instance" "terra_instance" {
-    ami = ami-084568db4383264d4
-    instance_type = t2.micro
+resource "aws_instance" "terra_instance2" {
+    ami = "ami-0df368112825f8d8f"
+    instance_type = "t2.micro"
     provider = aws.region2
     tags = {
         Name : "tfinstance2"
